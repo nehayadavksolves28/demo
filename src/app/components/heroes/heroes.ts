99575c5e20d2c3bb73c045fdcc5eeacdca685fa2
Neha Yadav <neha.yadav@ksolves.com>
2025-06-12 21:05:03 +0530

    hero datas showing

--- src/app/components/heroes/heroes.ts
+++ src/app/components/heroes/heroes.ts
@@ -1,17 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { Hero } from '../../interface/hero';
+import { UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HEROES } from '../../mock.heroes';
 
 @Component({
   selector: 'app-heroes',
-  imports: [],
+  imports: [UpperCasePipe,FormsModule],
   templateUrl: './heroes.html',
   styleUrl: './heroes.scss'
 })
 export class Heroes {
-    hero:Hero = {
-      id: 1,
-      name: 'Windstorm'
-    };
-    
+  heroes: Hero[] = HEROES;
     
 }
